d262912b13443edd17adede0a951d05b1086da00
siwpark <psw9428@naver.com>
2023-11-16 17:33:39 +0900

    punch_and_moving

--- left_right_moving/stepper_left_right/stepper_left_right.ino
+++ left_right_moving/stepper_left_right/stepper_left_right.ino
@@ -1,55 +1,59 @@
+#include <Servo.h>
 #include <Stepper.h>
+// #include <StaticThreadController.h>
+// #include <Thread.h>
+// #include <ThreadController.h>
 
-/*
- Stepper Motor Control - one revolution
-
- This program drives a unipolar or bipolar stepper motor.
- The motor is attached to digital pins 8 - 11 of the Arduino.
-
- The motor should revolve one revolution in one direction, then
- one revolution in the other direction.
-
-
- Created 11 Mar. 2007
- Modified 30 Nov. 2009
- by Tom Igoe
-
- */
-
-#include <Stepper.h>
+#define LEFT_PIN 2
+#define RIGHT_PIN 3
+#define PUNCH_PIN 4
 
 const int stepsPerRevolution = 200;  // change this to fit the number of steps per revolution
 // for your motor
+// ThreadController controll = ThreadController();
+
+// Thread thread1 = Thread();
+// Thread thread2 = Thread();
 
 // initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+//Stepper BigStepper(stepsPerRevolution, 8, 9, 10, 11);
+Servo myservo;
 
 void setup() {
   // set the speed at 60 rpm:
-  myStepper.setSpeed(30);
+  //BigStepper.setSpeed(60);
+
+  myservo.attach(9);
+  myservo.write(0);
+  
+  pinMode(LEFT_PIN, INPUT_PULLUP);
+  pinMode(RIGHT_PIN, INPUT_PULLUP);
+  pinMode(PUNCH_PIN, INPUT_PULLUP);
+
   // initialize the serial port:
-  pinMode(12, INPUT_PULLUP);
-  pinMode(7, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
 void loop() {
-  int a = digitalRead(12);
-  int b = digitalRead(7);
-  if (a == LOW && b == HIGH)
-    myStepper.step(20);
-  else if (b == LOW && a == HIGH)
-    myStepper.step(-20);
-
-
-  // // step one revolution  in one direction:
-  // Serial.println("clockwise");
-  // myStepper.step(stepsPerRevolution);
-  // delay(500);
-
-  // // step one revolution in the other direction:
-  // Serial.println("counterclockwise");
-  // myStepper.step(-stepsPerRevolution);
-  // delay(500);
+  int left = digitalRead(LEFT_PIN);
+  int right = digitalRead(RIGHT_PIN);
+  int punch = digitalRead(PUNCH_PIN);
+  if (left == LOW && right == HIGH)
+  {
+    Serial.println("left");
+    //BigStepper.step(20);
+  }
+  else if (right == LOW && left == HIGH)
+  {
+    Serial.println("right");
+    //BigStepper.step(-20);
+  }
+  if (punch == LOW) {
+    Serial.println("punch");
+    myservo.write(170);
+    delay(1000);
+    myservo.write(0);
+    delay(1000);
+  }
 }
 
---
+++ test_code/receive_signal/receive_signal.ino
@@ -0,0 +1,13 @@
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT_PULLUP);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, !digitalRead(2));
+}
